ac060fef4915d3ab2b4d2ec700a3526321d95b17
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-26 03:15:44 -0500

    test(scoring_schema): aminoacid_data readers works as expected

--- src/scoring_schema/aminacid_data.rs
+++ src/scoring_schema/aminacid_data.rs
@@ -1101,3 +1101,114 @@ pub const fn read_pam160(code_1: Aac, code_2: Aac) -> i8 {
         _ => panic!("Code not found. The table may be incomplete."),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const ALL_AAC: [Aac; 20] = [
+        Aac::A,
+        Aac::C,
+        Aac::D,
+        Aac::E,
+        Aac::F,
+        Aac::G,
+        Aac::H,
+        Aac::I,
+        Aac::K,
+        Aac::L,
+        Aac::M,
+        Aac::N,
+        Aac::P,
+        Aac::Q,
+        Aac::R,
+        Aac::S,
+        Aac::T,
+        Aac::V,
+        Aac::W,
+        Aac::Y,
+    ];
+
+    #[test]
+    fn check_some_blosum45() {
+        let score_cases = [
+            // Diagonal extremes and mid
+            (5, Aac::A, Aac::A),
+            (8, Aac::Y, Aac::Y),
+            (6, Aac::N, Aac::N),
+            // Check symmetria
+            (-2, Aac::P, Aac::I),
+            (-2, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, read_blosum45(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn blosum45_is_complete() {
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                read_blosum45(code_1, code_2);
+            }
+        }
+    }
+
+    #[test]
+    fn check_some_blosum62() {
+        let score_cases = [
+            // Diagonal extremes and mid
+            (4, Aac::A, Aac::A),
+            (7, Aac::Y, Aac::Y),
+            (6, Aac::N, Aac::N),
+            // Check symmetria
+            (-3, Aac::P, Aac::I),
+            (-3, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, read_blosum62(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn blosum62_is_complete() {
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                read_blosum62(code_1, code_2);
+            }
+        }
+    }
+
+    #[test]
+    fn check_some_pam160() {
+        let score_cases = [
+            // Diagonal extremes and mid
+            (2, Aac::A, Aac::A),
+            (8, Aac::Y, Aac::Y),
+            (3, Aac::N, Aac::N),
+            // Check symmetria
+            (-2, Aac::P, Aac::I),
+            (-2, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, read_pam160(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn paread_pam160_is_complete() {
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                read_pam160(code_1, code_2);
+            }
+        }
+    }
+}
