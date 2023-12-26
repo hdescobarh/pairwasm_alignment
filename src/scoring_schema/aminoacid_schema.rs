e01dfa5e654778419414ca07f0f6779382789943
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-26 03:44:17 -0500

    feat(scoring_schema): now the amino acid scoring matrices can be used in the scoring schema

--- src/scoring_schema/aminoacid_schema.rs
+++ src/scoring_schema/aminoacid_schema.rs
@@ -1,1 +1,147 @@
 //! Amino acid scoring schemas
+
+use super::aminoacid_data;
+use super::SubstitutionSchema;
+use crate::bioseq::Aac;
+
+struct Blosum45 {}
+
+impl SubstitutionSchema<Aac> for Blosum45 {
+    fn get_score(&self, code_1: Aac, code_2: Aac) -> i8 {
+        aminoacid_data::read_blosum45(code_1, code_2)
+    }
+}
+
+pub struct Blosum62 {}
+
+impl SubstitutionSchema<Aac> for Blosum62 {
+    fn get_score(&self, code_1: Aac, code_2: Aac) -> i8 {
+        aminoacid_data::read_blosum62(code_1, code_2)
+    }
+}
+
+struct Pam160 {}
+
+impl SubstitutionSchema<Aac> for Pam160 {
+    fn get_score(&self, code_1: Aac, code_2: Aac) -> i8 {
+        aminoacid_data::read_pam160(code_1, code_2)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const ALL_AAC: [Aac; 20] = [
+        Aac::A,
+        Aac::C,
+        Aac::D,
+        Aac::E,
+        Aac::F,
+        Aac::G,
+        Aac::H,
+        Aac::I,
+        Aac::K,
+        Aac::L,
+        Aac::M,
+        Aac::N,
+        Aac::P,
+        Aac::Q,
+        Aac::R,
+        Aac::S,
+        Aac::T,
+        Aac::V,
+        Aac::W,
+        Aac::Y,
+    ];
+
+    #[test]
+    fn check_some_blosum45() {
+        let blosum = Blosum45 {};
+
+        let score_cases = [
+            // Diagonal extremes and mid
+            (5, Aac::A, Aac::A),
+            (8, Aac::Y, Aac::Y),
+            (6, Aac::N, Aac::N),
+            // Check symmetria
+            (-2, Aac::P, Aac::I),
+            (-2, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, blosum.get_score(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn blosum45_is_complete() {
+        let blosum = Blosum45 {};
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                blosum.get_score(code_1, code_2);
+            }
+        }
+    }
+
+    #[test]
+    fn check_some_blosum62() {
+        let blosum = Blosum62 {};
+        let score_cases = [
+            // Diagonal extremes and mid
+            (4, Aac::A, Aac::A),
+            (7, Aac::Y, Aac::Y),
+            (6, Aac::N, Aac::N),
+            // Check symmetria
+            (-3, Aac::P, Aac::I),
+            (-3, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, blosum.get_score(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn blosum62_is_complete() {
+        let blosum = Blosum62 {};
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                blosum.get_score(code_1, code_2);
+            }
+        }
+    }
+
+    #[test]
+    fn check_some_pam160() {
+        let pam = Pam160 {};
+        let score_cases = [
+            // Diagonal extremes and mid
+            (2, Aac::A, Aac::A),
+            (8, Aac::Y, Aac::Y),
+            (3, Aac::N, Aac::N),
+            // Check symmetria
+            (-2, Aac::P, Aac::I),
+            (-2, Aac::I, Aac::P),
+            (-2, Aac::Q, Aac::G),
+            (-2, Aac::G, Aac::Q),
+        ];
+
+        for (expected, code_1, code_2) in score_cases {
+            assert_eq!(expected, pam.get_score(code_1, code_2))
+        }
+    }
+
+    #[test]
+    fn paread_pam160_is_complete() {
+        let pam = Pam160 {};
+        for code_1 in ALL_AAC {
+            for code_2 in ALL_AAC {
+                pam.get_score(code_1, code_2);
+            }
+        }
+    }
+}
--- src/scoring_schema/mod.rs
+++ src/scoring_schema/mod.rs
@@ -1,10 +1,12 @@
 //! Scoring schemas used for creating cost and distance functions
-mod aminacid_data;
-pub mod aminoacid_schema;
-pub mod gap_penalty;
-pub mod substitution; // delete this
+mod aminoacid_data;
+mod aminoacid_schema;
+mod gap_penalty;
+mod substitution; // delete this
 
+use crate::bioseq::Aac;
 use crate::utils::AlignmentUnit;
+use aminoacid_schema::*;
 use gap_penalty::*;
 
 pub trait SubstitutionSchema<A>
@@ -14,11 +16,21 @@ where
     fn get_score(&self, code_1: A, code_2: A) -> i8;
 }
 
-pub struct SchoringSchema<S, P>
+pub struct ScoringSchema<S, P>
 where
     P: Penalty,
     S: Sized,
 {
     score_table: S,
     gap: P,
+}
+
+impl<S, P> ScoringSchema<S, P>
+where
+    S: SubstitutionSchema<Aac>,
+    P: Penalty,
+{
+    pub fn new(score_table: S, gap: P) -> Self {
+        Self { score_table, gap }
+    }
 }
