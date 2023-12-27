315b50fdc81c2f3f18cc4ada72401e505c2e1fe4
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-12-27 15:06:31 -0500

    Revert "feat(scoring_schema): now have structs to deal with recoverable errors"
    This reverts commit cca42725bf7e16fdd2b6f973a2be0b0696807a9f.
    
    refactor(scoring_schema): Errors related to scoring schema should be unrecoverable

--- src/scoring_schema/gap_penalty.rs
+++ src/scoring_schema/gap_penalty.rs
@@ -2,9 +2,7 @@
 // A gap is a spaces chain, the lenght is the number of spaces. So,
 // the minimum possible lenght of a gap is 1, then the lenght >= 1
 
-use super::ErrorKind;
 use super::GapPenalty;
-use super::SchemaError;
 
 // This must be a primitive float.
 type CostType = f64;
@@ -26,11 +24,8 @@ impl Affine {
 }
 
 impl GapPenalty for Affine {
-    fn function(&self, lenght: usize) -> Result<CostType, SchemaError> {
-        if lenght == 0 {
-            return Err(SchemaError::new(ErrorKind::InvalidLenght));
-        };
-        Ok(self.open_cost + (self.extend_cost * lenght as CostType))
+    fn function(&self, lenght: usize) -> CostType {
+        self.open_cost + (self.extend_cost * lenght as CostType)
     }
     fn open(&self) -> CostType {
         self.open_cost
@@ -56,11 +51,8 @@ impl Linear {
 }
 
 impl GapPenalty for Linear {
-    fn function(&self, lenght: usize) -> Result<CostType, SchemaError> {
-        if lenght == 0 {
-            return Err(SchemaError::new(ErrorKind::InvalidLenght));
-        }
-        Ok(self.extend_cost * lenght as CostType)
+    fn function(&self, lenght: usize) -> CostType {
+        self.extend_cost * lenght as CostType
     }
 
     fn open(&self) -> CostType {
@@ -75,9 +67,4 @@ impl GapPenalty for Linear {
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn affine() {
-        let penalty = Affine::new(0.5, 10.0);
-    }
 }
