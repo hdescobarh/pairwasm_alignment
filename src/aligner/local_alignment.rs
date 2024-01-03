a81c00330b29e37e0de227c28a20ef520ba58b47
Hans D. Escobar H <hdescobarh@unal.edu.co>
2024-01-02 19:48:35 -0500

    feat(SmithWaterman): now can perform Local alignments with the Smith-Waterman original algorithm

--- src/aligner/local_alignment.rs
+++ src/aligner/local_alignment.rs
@@ -8,6 +8,7 @@ use crate::scoring_schema::gap_penalty::PenaltyKind;
 use crate::scoring_schema::AaScoringSchema;
 use crate::{scoring_schema::ScoringSchema, utils::AlignmentUnit};
 
+/// Smith Waterman original algorithm.
 pub struct SmithWaterman<'a, A>
 where
     A: AlignmentUnit,
@@ -16,8 +17,10 @@ where
     sequence_top: &'a dyn HasSequence<A>,
     scoring_schema: Box<dyn ScoringSchema<A>>,
     matrix: Matrix<BackTrack>,
-    maximum_score: f32,
-    starting_indices: Vec<[usize; 2]>,
+    /// The highest found score
+    global_maximum: f32,
+    /// Indices whose score is the global maximum
+    maximum_indices: Vec<[usize; 2]>,
 }
 
 impl<'a, A> SmithWaterman<'a, A>
@@ -25,15 +28,74 @@ where
     A: AlignmentUnit,
 {
     pub fn run(&mut self) -> Vec<AlignmentSequence<A>> {
-        todo!()
+        self.initialize();
+        self.solve_subproblems();
+
+        let mut all_paths: Vec<Vec<[usize; 2]>> = Vec::new();
+
+        for [init_row, init_col] in &self.maximum_indices {
+            let mut x = BackTrack::backtracking(&self.matrix, *init_row, *init_col, 0.0);
+            all_paths.append(&mut x);
+        }
+
+        let mut alignments: Vec<AlignmentSequence<A>> =
+            Vec::with_capacity(all_paths.len());
+        for backtrack_path in all_paths {
+            let new_alignment = AlignmentSequence::new(
+                backtrack_path,
+                self.sequence_left,
+                self.sequence_top,
+            );
+            alignments.push(new_alignment);
+        }
+        alignments
     }
 
     fn initialize(&mut self) {
-        todo!()
+        self.matrix[[0, 0]] = BackTrack::D(0.0);
+        let [rows, cols] = self.matrix.dim();
+
+        for i in 1..rows {
+            self.matrix[[i, 0]] = BackTrack::T(0.0);
+        }
+
+        for j in 1..cols {
+            self.matrix[[0, j]] = BackTrack::L(0.0);
+        }
     }
 
     fn solve_subproblems(&mut self) {
-        todo!()
+        let [rows, cols] = self.matrix.dim();
+        for i in 1..rows {
+            for j in 1..cols {
+                let diagonal = Self::diagonal_score(
+                    self.sequence_left,
+                    self.sequence_top,
+                    &self.scoring_schema,
+                    &self.matrix,
+                    i,
+                    j,
+                );
+                let top = Self::top_score(&self.scoring_schema, &self.matrix, i, j);
+                let left = Self::left_score(&self.scoring_schema, &self.matrix, i, j);
+
+                let (backtrack, current_maximum) =
+                    BackTrack::make_backtrack(top, diagonal, left);
+                self.update_maximum_entries(current_maximum, i, j);
+                self.matrix[[i, j]] = backtrack;
+            }
+        }
+    }
+
+    fn update_maximum_entries(&mut self, current_maximum: f32, i: usize, j: usize) {
+        // This comparisons may need to be improved because similarity is calculated with subtractions
+        // and they are ill-conditioned.
+        if current_maximum > self.global_maximum {
+            self.global_maximum = current_maximum;
+            self.maximum_indices = vec![[i, j]]
+        } else if current_maximum == self.global_maximum {
+            self.maximum_indices.push([i, j])
+        }
     }
 }
 
@@ -63,8 +125,8 @@ impl<'a> SmithWaterman<'a, Aac> {
             sequence_top,
             scoring_schema: scoring_schema as Box<dyn ScoringSchema<Aac>>,
             matrix: Matrix::full(BackTrack::Empty, rows, cols),
-            maximum_score: f32::NEG_INFINITY,
-            starting_indices: Vec::new(),
+            global_maximum: f32::NEG_INFINITY,
+            maximum_indices: Vec::new(),
         }
     }
 }
