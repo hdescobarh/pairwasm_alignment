6c0083597f3c75a1fa1efb26900f7ce659bb25e4
Hans D. Escobar H <hdescobarh@unal.edu.co>
2024-01-26 17:32:06 -0500

    feat: added error type for JS input options

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub mod tests;
 
 use aligner::AlignerKind;
 use scoring_schema::{aminoacid_schema::AaScoringKind, gap_penalty::PenaltyKind};
+use std::{error, fmt};
 use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
@@ -32,17 +33,18 @@ pub fn do_protein_alignment(
     let sequence_2 = Protein::new(string_2)?;
 
     let penalty_kind = PenaltyKind::Affine(open_cost, extend_cost);
+
     let score_kind = match substitution_matrix {
         b'\x01' => AaScoringKind::Blosum45,
         b'\x02' => AaScoringKind::Blosum62,
         b'\x03' => AaScoringKind::Pam160,
-        _ => panic!("Invalid option"),
+        _ => Err(InputError::new(InputErrorKind::ScoringMatrixNotExist))?,
     };
 
     let aligner_kind = match algorithm {
         b'\x01' => AlignerKind::SmithWaterman,
         b'\x02' => AlignerKind::NeedlemanWunsch,
-        _ => panic!("Invalid option"),
+        _ => Err(InputError::new(InputErrorKind::AlignerNotExist))?,
     };
 
     let mut aligner_instance = aligner::aminoacid_align_builder(
@@ -58,3 +60,47 @@ pub fn do_protein_alignment(
         .into_iter()
         .fold(String::new(), |acc, e| acc + &format!("{}", e)))
 }
+
+#[derive(Debug)]
+/// Error type for input operations.
+pub struct InputError {
+    kind: InputErrorKind,
+    message: String,
+}
+
+#[non_exhaustive]
+#[derive(Debug, PartialEq)]
+/// A list specifying general error categories of InputError.
+pub enum InputErrorKind {
+    AlignerNotExist,
+    GapModelNotExist,
+    ScoringMatrixNotExist,
+}
+
+impl InputError {
+    fn new(kind: InputErrorKind) -> Self {
+        let mut message: String = match kind {
+            InputErrorKind::AlignerNotExist => {
+                "The chosen aligner algorithm does not exist.".to_string()
+            }
+            InputErrorKind::GapModelNotExist => {
+                "The chosen gap model does not exist.".to_string()
+            }
+            InputErrorKind::ScoringMatrixNotExist => {
+                "The chosen scoring matrix does not exist.".to_string()
+            }
+        };
+
+        message.push_str(" Please check the documentation for more information.");
+
+        Self { kind, message }
+    }
+}
+
+impl fmt::Display for InputError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({:?}) {}", self.kind, self.message)
+    }
+}
+
+impl error::Error for InputError {}
